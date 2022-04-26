a4a7ae92cdda0e5d8608ceed528d888cc4f03a5a
Thomas Byr <tbouyer2000@gmail.com>
2022-04-26 14:11:46 +0200

    time.thread_time_ns() & big int format

--- Apprentissage_automatique/exo9b.py
+++ Apprentissage_automatique/exo9b.py
@@ -35,14 +35,14 @@ print("Feature ranking:")
 for f in range(x.shape[1]):
     print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
 
-t1 = time.clock()
+t1 = time.thread_time_ns()
 s = SelectFromModel(forest, prefit=True, max_features=3)
 x_new = s.transform(x)
 print(x_new.shape)
 print(x.shape)
-print("time for SelectFromModel: {}".format(time.clock() - t1))
+print(f"time for SelectFromModel: {(time.thread_time_ns() - t1):,}ns")
 
-t1 = time.clock()
+t1 = time.thread_time_ns()
 forest = RandomForestClassifier(n_estimators=40,
                                 class_weight='balanced_subsample',
                                 criterion='entropy',
@@ -51,12 +51,12 @@ s = RFE(forest, n_features_to_select=3)
 s = s.fit(x, y)
 print(s.support_)
 print(s.ranking_)
-print("time for RFE: {}".format(time.clock() - t1))
+print(f"time for RFE: {(time.thread_time_ns() - t1):,}ns")
 
-t1 = time.clock()
+t1 = time.thread_time_ns()
 s = RFECV(forest, cv=3, scoring=make_scorer(balanced_accuracy_score, needs_proba=False))
 s = s.fit(x, y)
 print(s.support_)
 print(s.ranking_)
-print("time for RFECV: {}".format(time.clock() - t1))
+print(f"time for RFECV: {(time.thread_time_ns() - t1):,}ns")
 #SequentialFeatureSelector from version 0.24
