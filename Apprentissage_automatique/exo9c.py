6cf659c6bcf298990527b8a88e33ef306017382f
Thomas Byr <tbouyer2000@gmail.com>
2022-04-26 15:29:56 +0200

    still doesn't work but better

--- Apprentissage_automatique/exo9c.py
+++ Apprentissage_automatique/exo9c.py
@@ -27,29 +27,48 @@ forest = RandomForestClassifier(
     max_samples=0.4,
 )
 forest.fit(x, y)
-model = SelectFromModel(forest, prefit=True)
-x_new = model.transform(x)
+
+pipeline = SelectFromModel(forest, prefit=True)
+x_new = pipeline.transform(x)
 print(x_new.shape)
 print(x.shape)
 
-res = []
-skf = StratifiedKFold(n_splits=3, shuffle=True, random_state=3)
-for train_index, test_index in skf.split(x, y):
-    X_train, X_test = x[train_index], x[test_index]
-    y_train, y_test = y[train_index], y[test_index]
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
 
-    forest = RandomForestClassifier(
-        n_estimators=100,
-        class_weight='balanced_subsample',
-        criterion='entropy',
-        min_samples_split=20,
-        max_features='sqrt',
-        max_samples=0.4,
-    )
+scores = []
+for train, test in kfold.split(x_new, y):
+    forest.fit(x_new[train], y[train])
+    predictions = forest.predict(x_new[test])
+    score = balanced_accuracy_score(y[test], predictions)
+    scores.append(score)
 
-    forest.fit(model.transform(X_train), y_train)
-    y_pred = forest.predict(model.transform(X_test))
-    res.append(balanced_accuracy_score(y_test, y_pred))
+print("Balanced accuracy: %.3f%% (+/- %.3f%%)" % (np.mean(scores), np.std(scores)))
 
-print(np.mean(res))
-print(np.std(res))
+# forest.fit(x, y)
+
+# model = SelectFromModel(forest, prefit=True)
+# x_new = model.transform(x)
+# print(x_new.shape)
+# print(x.shape)
+
+# res = []
+# skf = StratifiedKFold(n_splits=3, shuffle=True, random_state=3)
+# for train_index, test_index in skf.split(x, y):
+#     x_train, x_test = x[train_index], x[test_index]
+#     y_train, y_test = y[train_index], y[test_index]
+
+#     forest = RandomForestClassifier(
+#         n_estimators=100,
+#         class_weight='balanced_subsample',
+#         criterion='entropy',
+#         min_samples_split=20,
+#         max_features='sqrt',
+#         max_samples=0.4,
+#     )
+
+#     forest.fit(model.transform(x_train), y_train)
+#     y_pred = forest.predict(model.transform(x_test))
+#     res.append(balanced_accuracy_score(y_test, y_pred))
+
+# print(np.mean(res))
+# print(np.std(res))
