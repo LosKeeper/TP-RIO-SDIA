acc37b3690992523810db9c470ed90f1f87ac591
Thomas Byr <tbouyer2000@gmail.com>
2022-05-05 13:34:25 +0200

    latest

--- Apprentissage_non_supervise/bouyer/exo3/partie3.m
+++ Apprentissage_non_supervise/bouyer/exo3/partie3.m
@@ -4,7 +4,9 @@ K = 0;
 iter_per_step = 1;
 max_steps = 30;
 
-[new_centroids, new_labels] = kmeans(data, K, centroids, iter_per_step);
+sigma = eye(K);
+
+[new_centroids, sigma ,new_labels] = gmm_em(data, K, centroids, sigma,iter_per_step);
 
 fig3 = figure('Name', 'CLUSTERING');
 clf(fig3);
